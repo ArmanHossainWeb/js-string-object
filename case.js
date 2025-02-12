c9afb05a45e21fb3a8a0a65c32c18cf5e760135d
ArmanHossain04 <armanhossain0477@gmail.com>
2025-02-12 14:41:01 +0600

    added some file

---
+++ case.js
@@ -0,0 +1,44 @@
+const school = "RAJ UK Uttara Model School";
+console.log(school);
+
+console.log(school);
+
+console.log(school.toLowerCase());
+console.log(school.toUpperCase());
+
+
+
+mixedcase: ABcd;
+upparcase : ABCD;
+lowercase : abcd;
+
+
+
+
+
+const subject = "Chemisty";
+const book = "chemisty";
+
+if(subject.toLocaleLowerCase() === book.toLocaleLowerCase()){
+    console.log("i am reading book aibar porikkhay falai felmu ");
+}
+else{ 
+    console.log("huddai hudai prista ultai ar var khaware jonne ");
+}
+
+
+
+
+
+
+const drink = "water";
+const liquid = "    water";
+
+if (drink.trim() === liquid.trim()) {
+    console.log("pani er opor name life");
+    
+}
+else{
+    console.log("somudre pani ase khaite pari na");
+    
+}
---
+++ string.js
@@ -0,0 +1,25 @@
+const county = "bangladesh";
+const divition = "dhaka";
+const district = `tejgaon`;
+const thana = new String("tejgaon")
+console.log(typeof county);
+console.log(typeof divition);
+console.log(typeof district);
+console.log(typeof thana);
+
+
+const numbers = [54, 98, 78, 21, 65];
+console.log(numbers.length);
+console.log(numbers[1]);
+
+
+const capital = "dha ka";
+console.log(capital.length);
+
+console.log(capital);
+capital[1]= "f";
+console.log(capital);
+
+
+
+
